f31850b6e760cf3a35bdab2f6d364f13ec4cf584
Brian McIntosh <BrianMcIntosh1@shaw.ca>
2012-11-07 09:01:32 -0800

    Most Chapter 9 exercises complete

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,11 +28,12 @@ describe "Authentication" do
 
   	describe "with valid information" do
   		let(:user) { FactoryGirl.create(:user) }
-  		before do
-  			fill_in "Email",    with: user.email
-  			fill_in "Password", with: user.password
-  			click_button "Sign in"
-  		end
+  		before { sign_in(user) }
+      #do
+  		#	fill_in "Email",    with: user.email
+  		#	fill_in "Password", with: user.password
+  		#	click_button "Sign in"
+  		#end
 
   		it { should have_selector('title', text: user.name) }
 
@@ -45,7 +46,11 @@ describe "Authentication" do
 
   		describe "followed by signout" do
   			before { click_link "Sign out" }
-  			it { should have_link('Sign in')}
+  			it { should have_link('Sign in') }
+        it { should_not have_link('Users',    href: users_path) }
+        it { should_not have_link('Profile',  href: user_path(user)) }
+        it { should_not have_link('Settings', href: edit_user_path(user)) }
+        it { should_not have_link('Sign out', href: signout_path) }
   		end
   	end
 
@@ -68,6 +73,20 @@ describe "Authentication" do
 
           it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name) 
+            end
           end
         end
       end
